ccfb2c165876ab7162f9adf36da15fc40d2c626b
Ethan Liu <ethan.liu@duke.edu>
2025-10-28 02:01:36 -0400

    restarting

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-const rootElement = document.getElementById('root')!;
-if (!rootElement.classList.contains('bg-gray-900')) {
-  rootElement.classList.add('bg-gray-900');
-}
-
-createRoot(rootElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
